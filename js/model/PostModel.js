cac9bdfff797cddf576a3a0793d1052af6afe45b
DeadBryam <bryamu.u@gmail.com>
2019-11-15 19:40:26 -0600

    create models

---
+++ js/model/PostModel.js
@@ -0,0 +1,7 @@
+class PostModel{
+  constructor(postId,title,user){
+    this.postId = postId;
+    this.userId = userId;
+    this.title = title;
+  }
+}
---
+++ js/model/UserModel.js
@@ -0,0 +1,7 @@
+class UserModel{
+  constructor(userid, name, gender){
+    this.userId = userId;
+    this.name = name;
+    this.gender = gender;
+  }
+}
